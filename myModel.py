53402d3bfc61c433f4eb92866d8010fc82fd1c36
Alfonso H <alfonso.hera@gmail.com>
2025-03-26 08:36:18 +0100

    Freezing the base model lead to the classifier not learning. Reverting to training the whole model.

--- myModel.py
+++ myModel.py
@@ -156,17 +156,7 @@ def loadModel(model_name):
         model.load_state_dict(state_dict)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
-    # Apply freezing strategy: Freeze all parameters in the model
-    for name, param in model.named_parameters():
-        param.requires_grad = False
-
-    # Unfreeze the classifier head parameters: prosody_mlp and fc_combined
-    for name, param in model.prosody_mlp.named_parameters():
-        param.requires_grad = True
-
-    for name, param in model.fc_combined.named_parameters():
-        param.requires_grad = True
-
+        
     # Optionally, verify which parameters are trainable
     trainable_params = [name for name, param in model.named_parameters() if param.requires_grad]
     print("Trainable parameters:", trainable_params)
